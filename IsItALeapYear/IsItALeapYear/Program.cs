73d510a7abdac1c2ac2f9bf559bba0b3f6313b5c
Rohan0310 <rpalkar@confederationcollege.ca>
2021-06-06 20:14:34 -0400

    Completed the leap year program

---
+++ IsItALeapYear/IsItALeapYear/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace IsItALeapYear
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public bool leapYear(int year)
+        {
+            if (year % 400 == 0)
+                return true;
+
+            if (year % 100 == 0)
+                return false;
+
+            if (year % 4 == 0)
+                return true;
+
+            return false;
+        }
+
+       
+    }
+}
